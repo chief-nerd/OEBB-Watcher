dc3a89815022f0552fbf247de7f3bae8a1daef83
JR <jakob.reiter@theventury.com>
2021-03-07 21:05:50 +0100

    Make Monitor as Default

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const program = new Command();
 program.version(packageJSON.version, '-v, --version');
 
 program
-	.command('monitor [train]')
-	.option("-i, --interval <time_in_s>", "Update Interval in seconds")
+	.command('monitor [train]', {isDefault: true})
+	.option("-i, --interval <time_in_seconds>", "Update Interval in seconds")
 	.description('Monitor an ÖBB train')
 	.action(monitorTrain);
 
